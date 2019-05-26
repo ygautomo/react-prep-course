4508f20f831a4fabd8e853ab6af10c223a29ece5
ygautomo <y_gautomo@yahoo.com>
2019-05-26 23:32:01 +0700

    Update Personal.js

--- src/Personal.js
+++ src/Personal.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 
 class Personal extends Component {
-    // HINT: Use props to display your personal info
-    constructor(name, email, occupation) {
-    	super();
-    	this.name = name;
-    	this.email = email;
-    	this.occupation = occupation;
-    }
-    render() {
-	    return (
-	    	<p className="Personal">
-	      	Hello World !<br>
-	        My Name is {this.props.name}<br>
-	        My Email is {this.props.email}<br>
-	        My Occupation is {this.props.occupation}
-	      	</p>
-	    );
-    }
+	// HINT: Use props to display your personal info
+	constructor(name, email, occupation) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.occupation = occupation;
+	}
+	render() {
+		return (
+			<p className="Personal">
+				Hello World <br>My Name is {this.props.name}<br>My Email is {this.props.email}<br>My Occupation is {this.props.occupation}
+			</p>
+		);
+	}
 
 }
 
