6f452c5949026f20a0acedf779592a7369f71ff5
ygautomo <y_gautomo@yahoo.com>
2019-05-26 23:05:03 +0700

    Update App.js

--- src/Personal.js
+++ src/Personal.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Personal extends Component {
     // HINT: Use props to display your personal info
     constructor(name, email, occupation) {
+    	super();
     	this.name = name;
     	this.email = email;
     	this.occupatio = occupation;
